33dc3cad0f7424a046e23b2db0b0141787ed8cbd
mathildedest <mathilde.destailleur@edu.devinci.fr>
2025-12-12 16:44:46 +0100

    binary tree

--- Projet C++/BinaryTree.cpp
+++ Projet C++/BinaryTree.cpp
@@ -1,4 +1,71 @@
 #include "BinaryTree.h"
+#include <iostream>
+#include <stdexcept>
 
-// empty: everything is in the header (template)
-//lui
+template <class T>
+BinaryTree<T>::BinaryTree() {
+    _depth = 0;
+}
+
+template <class T>
+int BinaryTree<T>::index(int n, int i) const {
+    // index de la ligne n : n*(n+1)/2
+    return (n * (n + 1)) / 2 + i;
+}
+
+template <class T>
+void BinaryTree<T>::setDepth(int depth) {
+    if (depth < 0) {
+        throw std::invalid_argument("depth must be non-negative");
+    }
+
+    _depth = depth;
+
+    // nombre total de noeuds: 1 + 2 + ... + (depth+1)
+    int total = (_depth + 1) * (_depth + 2) / 2;
+
+    _data.clear();
+    _data.resize(total);
+}
+
+template <class T>
+int BinaryTree<T>::getDepth() const {
+    return _depth;
+}
+
+template <class T>
+void BinaryTree<T>::setNode(int n, int i, const T& value) {
+    _data[index(n, i)] = value;
+}
+
+template <class T>
+T BinaryTree<T>::getNode(int n, int i) const {
+    return _data[index(n, i)];
+}
+
+template <class T>
+void BinaryTree<T>::display() const {
+    // affichage "en arbre" simple (indentation)
+    for (int n = 0; n <= _depth; n++) {
+        // espaces au début pour centrer un peu
+        for (int s = 0; s < (_depth - n); s++) {
+            std::cout << "  ";
+        }
+
+        for (int i = 0; i <= n; i++) {
+            std::cout << getNode(n, i);
+            if (i < n) {
+                std::cout << "   ";
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+/*
+  IMPORTANT :
+  On force la génération du code pour les types qu'on utilise dans le projet.
+  Dans votre CRRPricer, vous utilisez BinaryTree<double> et BinaryTree<bool>.
+*/
+template class BinaryTree<double>;
+template class BinaryTree<bool>;
--- Projet C++/BinaryTree.h
+++ Projet C++/BinaryTree.h
@@ -1,44 +1,22 @@
 #pragma once
 #include <vector>
-#include <iostream>
 
 template <class T>
 class BinaryTree {
 private:
     int _depth;
-    std::vector< std::vector<T> > _tree;
+    std::vector<T> _data;   // stockage 1D contigu
+
+    int index(int n, int i) const;  // convertit (n,i) -> index dans _data
 
 public:
-    BinaryTree() : _depth(0) {}
+    BinaryTree();
 
-    void setDepth(int depth) {
-        _depth = depth;
-        _tree.clear();
-        _tree.resize(_depth + 1);
-        for (int n = 0; n <= _depth; ++n) {
-            _tree[n].resize(n + 1);
-        }
-    }
+    void setDepth(int depth);
+    int getDepth() const;
 
-    int getDepth() const {
-        return _depth;
-    }
+    void setNode(int n, int i, const T& value);
+    T getNode(int n, int i) const;
 
-    void setNode(int n, int i, const T& value) {
-        _tree[n][i] = value;
-    }
-
-    T getNode(int n, int i) const {
-        return _tree[n][i];
-    }
-
-    void display() const {
-        for (int n = 0; n <= _depth; ++n) {
-            for (int i = 0; i <= n; ++i) {
-                std::cout << _tree[n][i] << " ";
-            }
-            std::cout << std::endl;
-        }
-    }
+    void display() const;
 };
-//
